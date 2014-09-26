83bd1221275fda12640342bbcc336a0547313250
Witek Galecki <super@witek.org>
2014-09-26 20:25:04 +0200

    Server app created

---
+++ config/db.js
@@ -0,0 +1,11 @@
+(function(module){
+  
+  'use strict';
+
+  var config = {
+    url: ''
+  };
+
+  module.exports = config;
+
+}(module));
---
+++ server.js
@@ -0,0 +1,37 @@
+(function() {
+
+  'use strict';
+
+  // modules
+
+  var bodyParser    = require('body-parser'),
+    express         = require('express'),
+    methodOverride  = require('method-override'),
+
+  // config
+
+    app = express(),
+    db = require('./config/db'),
+    port = process.env.port || 8080;
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.json({
+    type: 'application/vnd.api+json' }
+  ));
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(methodOverride('X-HTTP-Method-Override'));
+  app.use(express.static(__dirname + '/public'));
+
+  // routes
+
+  require('./server/routes')(app);
+
+  // start app
+
+  app.listen(port);
+  console.log('Listening on ' + port);
+
+
+}());
