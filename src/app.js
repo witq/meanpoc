e044dcfc6349f67809dcf0c018635f2e2d7384c1
Witek Galecki <w.galecki@clearcode.cc>
2014-09-30 16:08:21 +0200

    This and that

--- src/app.js
+++ src/app.js
@@ -2,14 +2,54 @@
 
   'use strict';
 
-  var AppController = function($log) {
+  var AppController, routes;
+
+  routes = [
+    {
+      href: '/one',
+      title: 'One',
+      templateUrl: 'views/one.html',
+      controller: 'OneViewController',
+      controllerAs: 'one'
+    },
+    {
+      href: '/two',
+      title: 'Two',
+      templateUrl: 'views/two.html',
+      controller: 'TwoViewController',
+      controllerAs: 'two'
+    }
+  ];
+
+  AppController = function($log) {
+
+    var app = this;
 
     $log.debug('AppController loaded');
+
+    app.routes = routes;
 
   };
 
   angular
-    .module('meanPocApp', [])
+    .module('meanPocApp', [
+      'ngRoute'
+    ])
+    .config([
+      '$routeProvider',
+      function($routeProvider) {
+
+        // angular.forEach(routes, function(route) {
+        //   $routeProvider.when(route, {
+        //     title: route.title,
+        //     templateUrl: route.templateUrl,
+        //     controller: route.controller,
+        //     controllerAs: route.controllerAs
+        //   });
+        // });
+
+      }
+    ])
     .controller('AppController', [
       '$log',
       AppController
